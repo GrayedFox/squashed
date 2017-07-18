920fa88c81fe8afe2fad1d175fd8bb3a5e7bedfa
Che Fisher <che.fisher@gmail.com>
2017-07-18 11:48:47 +0200

    relax travis check, mini version increase, better error logging, tidy up code

--- app.js
+++ app.js
@@ -17,15 +17,30 @@ const commandHandler = (command) => {
   })
 }
 
-// Updates the exitCode and logs errors form command promise rejections
+// Logs errors from promise rejections, including stack trace
 const errorHandler = (error) => {
-  console.error(error)
-  process.exit(1)
+  console.log(error.stack.replace(error.message, ''))
+  quit(error.message, 1)
 }
 
-// Log squashed messages in blue so they stand out
-const prettyLog = (message) => {
-  console.log(`\x1b[34m${message}\x1b[0m`)
+// Log pass messages in blue so they stand out, with padding
+const logPass = (message) => {
+  console.log(`
+  \x1b[34m${message}\x1b[0m
+  `)
+}
+
+// Log fail message in red so they stand out, with padding
+const logFail = (message) => {
+  console.log(`
+  \x1b[31m${message}\x1b[0m
+  `)
+}
+
+// Quit squashed with a given message and exit code
+const quit = (message, statusCode = 0) => {
+  statusCode === 0 ? logPass(message) : logFail(message)
+  process.exit(statusCode)
 }
 
 // Initialisation and env setup
@@ -40,7 +55,12 @@ const init = () => {
   }
 
   // if run inside Travis CI we need to ensure we update the commitRef since we are working in a detached head state
-  if (process.env.TRAVIS_PULL_REQUEST_BRANCH) {
+  if (process.env.TRAVIS) {
+    if (!process.env.TRAVIS_PULL_REQUEST_SHA) {
+      quit(`No need to run squashed against push builds!`)
+    } else if (process.env.TRAVIS_BRANCH !== 'develop') {
+      quit(`No need to run squashed against pull requests that don't target develop!`)
+    }
     commitRef = process.env.TRAVIS_PULL_REQUEST_SHA
   }
   console.log(`Commit Reference: ${commitRef}`)
@@ -51,14 +71,9 @@ init()
 Promise.all([commandHandler(`getBranchCommitCount.sh ${commitRef}`), commandHandler(`getFormattedBranchCommits.sh ${commitRef}`)]).then(
   ([commitCount, formattedBranchCommits]) => {
     if (commitCount > 1) {
-      console.log(`
-${formattedBranchCommits}`)
-      prettyLog(`
-      You need to squash those ${+commitCount} commits!
-      `)
-      process.exit(1)
+      console.log(`${formattedBranchCommits}`)
+      quit(`You need to squash those ${+commitCount} commits!`, 1)
     } else {
-      prettyLog('Merge away McLoven :)')
-      process.exit(0)
+      quit(`Merge away McLoven :)`)
     }
   }, errorHandler)
